25c8fd9a4e79788111baf2974e364d8b8fa75db1
HP <okoro99@mail.ru>
2022-08-11 23:07:08 +0300

    111

---
+++ src/TT.java
@@ -0,0 +1,10 @@
+public class TT {
+    public static void main(String[] args) {
+        System.out.printf("mamam");
+        System.out.printf("mamam");
+        System.out.printf("mamam");
+        System.out.printf("mamam");
+        System.out.printf("mamam");
+        System.out.printf("mamam");
+    }
+}
