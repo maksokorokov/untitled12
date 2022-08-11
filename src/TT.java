c3458d7e2203d02fe8abeada081434ed199e4b62
HP <okoro99@mail.ru>
2022-08-11 23:08:31 +0300

    22

--- src/TT.java
+++ src/TT.java
@@ -2,9 +2,5 @@ public class TT {
     public static void main(String[] args) {
         System.out.printf("mamam");
         System.out.printf("mamam");
-        System.out.printf("mamam");
-        System.out.printf("mamam");
-        System.out.printf("mamam");
-        System.out.printf("mamam");
     }
 }
